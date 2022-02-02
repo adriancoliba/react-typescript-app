571327b141f81f7f8d714cfc10d65e6da2c6e8f6
Adrian Coliba <adrian.coliba5@gmail.com>
2022-02-02 21:58:26 +0200

    add Button to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Button } from "./components";
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
         >
           Test TypeScript
         </a>
-        <h1 className="text-3xl font-bold underline bg-blue-500">
+        <h1 className="mt-3 text-3xl font-bold underline bg-blue-500">
           Hello Tailwind
         </h1>
+        <Button onClick={() => console.log("Button clicked")} size="small" variant="danger" className="mt-6">test button</Button>
       </header>
     </div>
   );
