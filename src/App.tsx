703973496f6e1eeca37d5ddb49303aaced16c27d
Adrian Coliba <adrian.coliba5@gmail.com>
2022-02-03 12:05:17 +0200

    change props from string to string literal types

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,9 @@ function App() {
         <h1 className="mt-3 text-3xl font-bold underline bg-blue-500">
           Hello Tailwind
         </h1>
-        <Button onClick={() => console.log("Button clicked")} size="small" variant="danger" className="mt-6">test button</Button>
+        <Button onClick={() => console.log("Small button clicked")} size="small" variant="primary" className="mt-6">Small primary</Button>
+        <Button onClick={() => console.log("Medium button clicked")} size="medium" variant="secondary" className="mt-6">Medium secondary</Button>
+        <Button onClick={() => console.log("Large button clicked")} size="large" variant="danger" className="mt-6">Large danger</Button>
       </header>
     </div>
   );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,12 +7,12 @@ const classes = {
     pill: "rounded-full",
     size: {
         small: "px-2 py-1 text-sm rounded",
-        normal: "px-4 py-2 rounded-md",
+        medium: "px-4 py-2 text-base rounded-md",
         large: "px-8 py-3 text-lg rounded-lg"
     },
     variant: {
         primary: "bg-blue-500 hover:bg-blue-800 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-white",
-        secondary: "bg-gray-200 hover:bg-gray-800 focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 text-gray-900 hover:text-white",
+        secondary: "bg-gray-200 hover:bg-gray-500 focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 text-gray-900 hover:text-white",
         danger: "bg-red-500 hover:bg-red-800 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 text-white"
     }
 }
@@ -21,8 +21,8 @@ interface Props {
     children: React.ReactNode;
     type?: "submit" | "button" | "reset";
     onClick: () => void;
-    variant?: string
-    size?: string,
+    variant?: "primary" | "secondary" | "danger"
+    size?: "small" | "medium" | "large",
     pill?: string,
     disabled?: boolean;
     className?: string;
@@ -33,7 +33,7 @@ const Button: React.FC<Props> = ({
     type = "button",
     onClick,
     variant = "primary",
-    size = "normal",
+    size = "medium",
     pill = "",
     disabled = false,
     className = "",
@@ -51,6 +51,7 @@ const Button: React.FC<Props> = ({
                 ${pill && classes.pill}
                 ${disabled && classes.disabled}
                 ${className}
+
             `)}
             {...rest}
         >
